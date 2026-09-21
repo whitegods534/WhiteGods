0272387c8ac57fbe21478dc04cd11455cc92ec4e
whitegods534 <jaydendavidjames1@outlook.com>
2026-09-21 19:28:11 +0100

    Add auth command for Roblox user authorization

--- src/commands/auth.js
+++ src/commands/auth.js
@@ -1,1 +1,30 @@
+import { SlashCommandBuilder } from 'discord.js';
 
+const FINANCIAL_OPERATIONS_ROLE = '1551601783581843497';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('auth')
+        .setDescription('Authorize a Roblox user')
+        .addStringOption(option =>
+            option
+                .setName('robloxuser')
+                .setDescription('The Roblox username to authorize')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        if (!interaction.member.roles.cache.has(FINANCIAL_OPERATIONS_ROLE)) {
+            return interaction.reply({
+                content: 'You do not have permission to use this command.',
+                ephemeral: true
+            });
+        }
+
+        const username = interaction.options.getString('robloxuser');
+
+        await interaction.reply({
+            content: `Authorization request received for **${username}**.`
+        });
+    }
+};
